fd5c9ed7398fbc6f02e9db66d8ab284a542fa40b
Michael Niessl <mniessl3@gatech.edu>
2019-05-02 19:42:00 +0200

    do not force https

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 from flask import Flask, request, make_response, render_template, abort
 import slack
-from flask_sslify import SSLify
 
 token = os.environ["SLACK_API_TOKEN"]
 slack_client = slack.WebClient(token=token)
 
 app = Flask(__name__)
-SSLify(app)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -20,7 +18,6 @@ def events():
         abort(400)
 
     return request.json["challenge"]
-
 
 
 if __name__ == '__main__':
