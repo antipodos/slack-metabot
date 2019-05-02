d6df51fad1d222d2296a713e83f365d87cb7c273
Michael Niessl <mniessl3@gatech.edu>
2019-05-02 18:46:46 +0200

    talisman wrapper for https, challenge response

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, request, make_response, render_template
+import os
+from flask import Flask, request, make_response, render_template, abort
+from flask_talisman import Talisman
+import slack
+
+token = os.environ["SLACK_API_TOKEN"]
+slack_client = slack.WebClient(token=token)
 
 app = Flask(__name__)
+Talisman(app)
 
 @app.route("/", methods=["GET"])
 def home():
-  return 'Hello World'
+    return "slack meta bot"
+
+
+@app.route("/event", methods=["POST"])
+def events():
+    if not request.json:
+        abort(400)
+
+    return request.json["challenge"]
+
 
 
 if __name__ == '__main__':
